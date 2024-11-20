9e2e0bab13c13ebac44b1f18bd1dcf89ec92dbc0
DED-LEGEND <159163158+DED-LEGEND@users.noreply.github.com>
2024-11-21 03:21:19 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -97,7 +97,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DED-LEGEND/LUCKY-MUSIC",
+    "https://github.com/DED-LEGEND/RAJJMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
